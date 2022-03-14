d3e73d1348812488192da7671b581f9cc996f2f8
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-14 17:49:47 +0530

    completed 06.reverseLL.cpp

--- LinkedList-Complete/06.reverseLL.cpp
+++ LinkedList-Complete/06.reverseLL.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
+/*
+LL = head -> 1 -> 2 -> 3 -> 4
+after reversal
+LL = 1 <- 2 <- 3 <- 4 <- head
 
+iterative appr - o(n) time + o(1) space
+
+*/
 class node{
 public:
     int data;
@@ -26,7 +33,36 @@ void printLL(node*head){
     }
     cout<<endl;
 }
-int main(){
+void reverseLL(node*&head){
+    if(head == NULL or head->next == NULL){
+        //no node or just one node
+        return;
+    }
+    //we will do this using previous, current and next pointers
+    node* prev = NULL;
+    node* curr = head;
+    node* next;
+    while(curr != NULL){
+        next = curr->next;
 
+        curr->next = prev; // make the current node point to previous node
+
+        //update the prev and curr pointers
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
+int main(){
+    node* head = NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    insertAtHead(head,2);
+    insertAtHead(head,1);
+    printLL(head);// output = 1->2->3->4->5->
+
+    reverseLL(head);
+    printLL(head);//output = 5->4->3->2->1->
     return 0;
 }

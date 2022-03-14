7076df41ebcffca7d3e835c79f286916bc9ce50a
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-14 21:14:27 +0530

    completed 10.mergeTwoSortedLL.cpp

--- LinkedList-Complete/10.mergeTwoSortedLL.cpp
+++ LinkedList-Complete/10.mergeTwoSortedLL.cpp
@@ -28,15 +28,47 @@ void printLL(node*head){
     }
     cout<<endl;
 }
+node* solve(node*a, node*b){
+    //merge two sorted LLs into one by rearranging the pointers using recursion
+    
+    //base case
+    if(a == NULL){
+        return b;
+    }
+    if(b == NULL){
+        return a;
+    }
 
+    //take a new head pointer
+    node*c; 
+    if(a->data < b->data){
+        c = a;
+        c->next = solve(a->next,b);
+    }
+    else{
+        c = b;
+        c->next = solve(a,b->next);
+    }
+    return c;
+}
 int main(){
-    node* head = NULL;
-    insertAtHead(head,5);
-    insertAtHead(head,4);
-    insertAtHead(head,3);
-    insertAtHead(head,2);
-    insertAtHead(head,1);
-    printLL(head);// output = 1->2->3->4->5->
+    node* head1 = NULL;
+    insertAtHead(head1,7);
+    insertAtHead(head1,4);
+    insertAtHead(head1,3);
+    insertAtHead(head1,1);
+    printLL(head1);// output = 1->3->4->7
 
+    node* head2 = NULL;
+    insertAtHead(head2,10);
+    insertAtHead(head2,8);
+    insertAtHead(head2,2);
+    insertAtHead(head2,0);
+    printLL(head2);
+
+    node* mergedLL = solve(head1,head2);
+    cout<<"Merged LL:"<<endl;
+    printLL(mergedLL);
+    
     return 0;
 }

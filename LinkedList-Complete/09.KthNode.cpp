d662d3e63fd036b8c1b26758a162bf36b6eb127d
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-14 18:34:33 +0530

    init 09.KthNode.cpp

---
+++ LinkedList-Complete/09.KthNode.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* next;
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+void insertAtHead(node*&head, int d){
+    if(head == NULL){
+        head = new node(d);
+        return;
+    }
+    node* temp = new node(d);
+    temp->next = head;
+    head = temp;
+}
+void printLL(node*head){
+    while(head != NULL){
+        cout<<head->data<<"->";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node* head = NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    insertAtHead(head,2);
+    insertAtHead(head,1);
+    printLL(head);// output = 1->2->3->4->5->
+
+    return 0;
+}

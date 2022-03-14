98e5f8b0983215ae39bc774c139f4f610112c74c
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-14 11:54:26 +0530

    added comments in 01.insertion.cpp

--- LinkedList-Complete/01.insertion.cpp
+++ LinkedList-Complete/01.insertion.cpp
@@ -21,26 +21,28 @@ public:
 };
 
 void insertAtHead(node* &head, int d){
+    //head's datatype is node* and it is passed by reference, therefore node*&head
     if(head == NULL){
         //first node insertion
         head = new node(d);
         return;
     }
     //insertion for remaining nodes
-    node* n = new node(d);
-    n->next = head;
-    head = n;
+    node* temp = new node(d); //create a temp node
+    temp->next = head; //temp's next will store the address stored in head
+    head = temp; // head will point to new node i.e. temp
 }
 void printLL(node*head){
+    //traversing the linked list
     while(head != NULL){
         cout<<head->data<<"->";
-        head = head->next;
+        head = head->next; //updating head
     }
     cout<<endl;
 }
 int main(){
     
-    node* head = NULL;
+    node* head = NULL; //initialising empty linked list
 
     insertAtHead(head,5);
     insertAtHead(head,4);

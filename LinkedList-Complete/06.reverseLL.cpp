8b0625f6de82b18306ef8ae3484a7459f0ecb26c
Raghav Agarwal <raghavagrawal406@gmail.com>
2022-03-14 16:59:47 +0530

    init 06.reverseLL.cpp

---
+++ LinkedList-Complete/06.reverseLL.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* next;
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+void insertAtHead(node*&head, int d){
+    if(head == NULL){
+        head = new node(d);
+        return;
+    }
+    node* temp = new node(d);
+    temp->next = head;
+    head = temp;
+}
+void printLL(node*head){
+    while(head != NULL){
+        cout<<head->data<<"->";
+        head = head->next;
+    }
+    cout<<endl;
+}
+int main(){
+
+    return 0;
+}
